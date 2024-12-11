979ac61362320d571c8ac037cb8fef3b5ab31622
parsahl0707 <hl0707@wayne.edu>
2024-12-11 17:28:08 -0500

    Fixed stage-11.sh

--- Development-Tools/HTTP-Requests/stages/stage-11.sh
+++ Development-Tools/HTTP-Requests/stages/stage-11.sh
@@ -1,8 +1,8 @@
 # Bill Request Revision
 
 auth/login.sh admin admin
-bill/post-bill-request-revision.sh '{
-"billRequestId": 11,
+bills/post-bill-request-revision.sh '{
+"billRequestId": 15,
 "price": 625,
 "note": "Is this good with you"
 }'
