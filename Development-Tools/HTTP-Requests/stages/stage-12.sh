70f7c39f1a37267d8d7b82118e693f71332e912d
parsahl0707 <hl0707@wayne.edu>
2024-12-11 17:40:06 -0500

    Added stage-12.sh

---
+++ Development-Tools/HTTP-Requests/stages/stage-12.sh
@@ -0,0 +1,9 @@
+# Bill Response Revisions
+
+auth/login.sh jacob jacob
+bills/post-bill-response-revision.sh '{
+"billResponseId": 14,
+"disputed": 0,
+"cardNumber": "5482014820148201",
+"note": "I am fine with that price"
+}'
